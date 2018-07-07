8805217affa962e0e01caaf8630e9676a78dbf01
tomoki ito <5cho38romu@gmail.com>
2018-07-07 15:42:01 +0900

    ログインしてるユーザーのみを使用可能にした

--- app/controllers/musical_instruments_controller.rb
+++ app/controllers/musical_instruments_controller.rb
@@ -1,6 +1,6 @@
 class MusicalInstrumentsController < ApplicationController
   before_action :set_musical_instrument, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
   # GET /musical_instruments
   # GET /musical_instruments.json
   def index
