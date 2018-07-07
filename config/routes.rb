ba4be709d1c16fb7a8589e8f4118636be0a270d1
tomoki ito <5cho38romu@gmail.com>
2018-07-07 15:20:07 +0900

    gem 'devise'基礎部分の完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'musical_instruments#index'
+
   resources :musical_instruments
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
